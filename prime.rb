5bb6cc676e95ba591cbf823544c8ffb99b7b1a53
Olivia Kasten <odkasten@gmail.com>
2018-09-14 22:19:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false 
+  end 
+  
+  divisor = 3 
+  if num > 2 && num.even?
+    return false 
+  else
+    while divisor < num 
+      if num % divisor == 0 
+        return false 
+      else 
+        divisor +=1 
+      end 
+    end 
+  end 
+  return true 
+end 
